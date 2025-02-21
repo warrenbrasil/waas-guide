4435c980df2ed3576064a6e7ae4328f53c0f7501
Rafael Neves <rafael.neves@warren.com.br>
2025-02-20 22:43:58 -0300

    ajusta logo url

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,22 +2,22 @@ import { GlobalState } from "@/types";
 
 export default {
   branding: {
-    logo: '/logo.svg',
+    logo: '/guide/logo.svg',
     name: 'Guide',
     theme: {
       dark: {
-        logo: '/guide-logo-white.png',
+        logo: '/guide/guide-logo-white.png',
         logoSize: 'h-8'
       },
       light: {
-        logo: '/guide-logo-black.png',
+        logo: '/guide/guide-logo-black.png',
         logoSize: 'h-8'
       }
     }
   },
   theme: 'light',
   specs: {
-    warp: '/specs/warp.json',
+    warp: '/guide/specs/warp.json',
     "swagger petstore - openapi 3.0": 'https://petstore3.swagger.io/api/v3/openapi.json',
     "spoonacular api": "https://spoonacular.com/application/frontend/downloads/spoonacular-openapi-3.json"
   },
